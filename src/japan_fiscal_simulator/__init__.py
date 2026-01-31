b4ec687291d1d2341e93314fb1a3e1525f405ad2
DaisukeYoda <daisukeyoda@users.noreply.github.com>
2026-02-01 02:54:23 +0900

    Bump version to 0.0.2

--- src/japan_fiscal_simulator/__init__.py
+++ src/japan_fiscal_simulator/__init__.py
@@ -1,6 +1,6 @@
 """Japan Fiscal Simulator - 日本財政政策DSGEシミュレーター"""
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 from japan_fiscal_simulator.core.model import DSGEModel
 from japan_fiscal_simulator.core.simulation import ImpulseResponseSimulator
